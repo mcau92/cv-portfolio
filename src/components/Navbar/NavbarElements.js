5fcb4d771eb120069c0ca8a175a319c9742168f1
Michael Cauduro <Michael.Cauduro@contractors.cifgroup.com>
2021-02-12 17:43:51 +0100

    ttfni

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { Link as LinkR } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav`
-  background: ${({ scrollNav }) => (scrollNav ? "#181818" : "transparent")};
+  background: ${({ scrollNav }) => (scrollNav ? "#181818" : "red")};
   transition: background-color 1s ease 0s;
   height: 80px;
   width: 100%;
