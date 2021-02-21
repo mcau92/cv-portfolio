4f64df81823abf053a93a51838d561da5e3dc3d9
Michael Cauduro <Michael.Cauduro@contractors.cifgroup.com>
2021-02-21 15:15:07 +0100

    dev on creation of experience section

--- src/components/Experiences/ExperiencesElements.js
+++ src/components/Experiences/ExperiencesElements.js
@@ -1,57 +1,73 @@
 import styled from "styled-components";
 
-export const ExperienceContainer = styled.div``;
+export const ExperienceContainer = styled.div`
+  background: white;
+`;
 
 export const ExperienceSection = styled.div`
-  background: #0c0c0c;
   padding: 5% 15%;
 
   @media screen and (max-width: 780px) {
     padding: 5% 8%;
   }
+  @media screen and (max-width: 520px) {
+    padding: 5% 2%;
+  }
 `;
-export const WorkContainer = styled.div`
-  background: red;
+export const ExperienceSectionDivWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 5px;
+  overflow: hidden;
+  background-color: #f9f9f9;
 `;
+export const WorkContainer = styled.div``;
 export const EducationContainer = styled.div`
-  background: green;
+  border-top: thin solid lightgray;
 `;
 
 //work
 export const WorkTitleH3 = styled.h3`
   margin: 0;
   text-align: left;
-  padding: 5px;
+  font-weight: bold;
+  padding: 20px 15px;
+  padding-bottom: 10px;
   color: rgb(241, 113, 27);
 `;
 
 export const WorkWrapper = styled.div`
-  background: white;
   display: grid;
-  grid-template-columns: 110px auto;
+  grid-template-columns: 20% auto;
   grid-template-areas: "col1 col2" "col3 col4";
+  padding-bottom: 25px;
 `;
 export const Col1 = styled.div`
   display: flex;
-  padding: 5px;
+  padding: 15px;
+  justify-content: center;
+  align-items: center;
   grid-area: col1;
 `;
-export const WorkImage = styled.img`
-  min-height: 50px;
-  max-height: 75px;
-  max-width: 100px;
+export const Image = styled.img`
+  max-height: 55%;
+  max-width: 5rem;
+  @media screen and (max-width: 520px) {
+    max-width: 3.5rem;
+  }
 `;
 export const Col2 = styled.div`
-  padding: 25px;
-  padding-left: 5px;
+  padding: 25px 5px;
   grid-area: col2;
 `;
 export const WorkTitle = styled.h4`
   margin: 0;
   text-align: left;
+  padding: 5px 0;
   font-weight: 900;
 `;
 export const WorkDuration = styled.h5`
+  font-weight: normal;
   margin: 0;
   text-align: left;
 `;
@@ -66,6 +82,7 @@ export const VerticalLine = styled.div`
 export const Col4 = styled.div`
   align-items: flex-start;
   padding: 5px;
+  padding-right: 20px;
   grid-area: col4;
 `;
 export const WorkDescription = styled.div`
@@ -82,7 +99,41 @@ export const WorkDescriptionTitle = styled.h4`
 export const WorkDescriptionDuration = styled.h5`
   margin: 0;
   text-align: left;
+  font-weight: normal;
   letter-spacing: 1px;
 `;
 
-//
+//education
+
+export const EducationTitleH3 = styled.h3`
+  margin: 0;
+  text-align: left;
+  font-weight: bold;
+  padding: 20px 15px;
+  padding-bottom: 10px;
+  color: rgb(241, 113, 27);
+`;
+
+export const EducationWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 20% auto;
+  grid-template-areas: "col1 col2" "col3 col4";
+  padding-bottom: 25px;
+`;
+export const EducationTitle = styled.h4`
+  margin: 0;
+  text-align: left;
+  padding: 5px 0;
+  font-weight: 900;
+`;
+export const EducationDescription = styled.h5`
+  font-weight: normal;
+  margin: 0;
+  text-align: left;
+`;
+export const Col2BorderBottom = styled.div`
+  padding: 25px;
+  padding-left: 5px;
+  grid-area: col2;
+  border-bottom: thin solid lightgray;
+`;
--- src/components/Experiences/index.js
+++ src/components/Experiences/index.js
@@ -6,7 +6,7 @@ import {
   WorkContainer,
   EducationContainer,
   WorkTitleH3,
-  WorkImage,
+  Image,
   WorkWrapper,
   Col1,
   Col2,
@@ -18,8 +18,16 @@ import {
   WorkDescriptionTitle,
   WorkDescriptionDuration,
   VerticalLine,
+  ExperienceSectionDivWrapper,
+  EducationTitleH3,
+  EducationWrapper,
+  EducationTitle,
+  EducationDescription,
+  Col2BorderBottom,
 } from "./ExperiencesElements";
 import euris from "../../images/euris.png";
+import uninettuno from "../../images/uninettuno-logo.jpg";
+import hschool from "../../images/hs-logo.png";
 const Experiences = () => {
   const eurisDate = getEurisDate();
 
@@ -27,42 +35,70 @@ const Experiences = () => {
     <>
       <ExperienceContainer id="experiences_skills">
         <ExperienceSection>
-          <WorkContainer>
-            <WorkTitleH3>..My Working Experiences</WorkTitleH3>
-            <WorkWrapper>
-              <Col1>
-                <WorkImage src={euris} />
-              </Col1>
-              <Col2>
-                <WorkTitle>Gruppo Euris Spa</WorkTitle>
-                <WorkDuration>{eurisDate}</WorkDuration>
-              </Col2>
-              <Col3>
-                <VerticalLine />
-              </Col3>
-              <Col4>
-                <WorkDescription>
-                  <WorkDescriptionTitle>
-                    Java Software Developer
-                  </WorkDescriptionTitle>
-                  <WorkDescriptionDuration>
-                    november 2018-present
-                  </WorkDescriptionDuration>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </WorkDescription>
-              </Col4>
-            </WorkWrapper>
-          </WorkContainer>
-          <EducationContainer>Education section</EducationContainer>
+          <ExperienceSectionDivWrapper>
+            <WorkContainer>
+              <WorkTitleH3>Work Experiences</WorkTitleH3>
+              <WorkWrapper>
+                <Col1>
+                  <Image src={euris} />
+                </Col1>
+                <Col2>
+                  <WorkTitle>Gruppo Euris Spa</WorkTitle>
+                  <WorkDuration>{eurisDate}</WorkDuration>
+                </Col2>
+                <Col3>
+                  <VerticalLine />
+                </Col3>
+                <Col4>
+                  <WorkDescription>
+                    <WorkDescriptionTitle>
+                      Java Software Developer
+                    </WorkDescriptionTitle>
+                    <WorkDescriptionDuration>
+                      november 2018-present
+                    </WorkDescriptionDuration>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </WorkDescription>
+                </Col4>
+              </WorkWrapper>
+            </WorkContainer>
+            <EducationContainer>
+              <EducationTitleH3>Education Experiences</EducationTitleH3>
+              <EducationWrapper>
+                <Col1>
+                  <Image src={uninettuno} />
+                </Col1>
+                <Col2BorderBottom>
+                  <EducationTitle>
+                    Università Telematica Internazionale Uninettuno
+                  </EducationTitle>
+                  <EducationDescription>
+                    Bachelor's degree - Computer engineering
+                  </EducationDescription>
+                </Col2BorderBottom>
+              </EducationWrapper>
+              <EducationWrapper>
+                <Col1>
+                  <Image src={hschool} />
+                </Col1>
+                <Col2>
+                  <EducationTitle>I.T.S. Luigi Einaudi</EducationTitle>
+                  <EducationDescription>
+                    Secondary School Diploma - IT Engineer
+                  </EducationDescription>
+                </Col2>
+              </EducationWrapper>
+            </EducationContainer>
+          </ExperienceSectionDivWrapper>
         </ExperienceSection>
         <Software />
       </ExperienceContainer>
